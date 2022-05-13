7ef8705c8153923690811041def59cd1c2d6a614
Ricardo陈 <96281398+CKIVZy@users.noreply.github.com>
2022-05-13 15:54:50 +0800

    feat:添加了useMount和useDebounced

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,7 +1,7 @@
 import { List } from "./list"
 import { SearchPanel } from "./searc-panel"
 import {useState,useEffect} from 'react'
-import {cleanObject} from '../../utils'
+import {cleanObject, useDebounce, useMount} from '../../utils'
 import * as qs from 'qs'
 const apiUrl = process.env.REACT_APP_API_URL
 export const ProjectListScreen = ()=>{
@@ -11,26 +11,26 @@ export const ProjectListScreen = ()=>{
     })
     const [list,setList] = useState([])
     const [users,setUsers] = useState([])
+    const debouncedParams = useDebounce(params,2000)
     useEffect(()=>{
-        // 
         /* 
             qs.stringify 是把一个参数对象格式化为一个字符串
             qs.stringify({ c: 'b', a: 'd' })
             'c=b&a=d'
          */
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(params))}`).then(async response =>{
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParams))}`).then(async response =>{
             if(response.ok){
                 setList(await response.json())
             }
         })
-    },[params])
-    useEffect(()=>{
+    },[debouncedParams])
+    useMount(()=>{
         fetch(`${apiUrl}/users`).then(async response=>{
             if(response.ok){
                 setUsers(await response.json())
             }
         })
-    },[])
+    })
     return <div>
         <SearchPanel users={users} params={params} setParams={setParams}/>
         <List users={users} list={list}/>
--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,9 +1,11 @@
+import { useEffect, useState } from "react"
+
 export const isFalsy = (value) =>value === 0? false :!value
 //一个！求反 2个！！转布尔
 //在一个函数里，改变传入的对象本身是不好的
 export const cleanObject = (object)=>{
     //等价于 Object.assign({},object)
-    const result = {...object}
+    const result = {...object} 
     Object.keys(result).forEach(key=>{
         const value = object[key]
         if(isFalsy(value)){
@@ -13,4 +15,32 @@ export const cleanObject = (object)=>{
         }
     })
     return result
+}
+
+export const useMount = (callback)=>{
+    useEffect(()=>{
+        callback()
+    },[])
+}
+//防抖
+// const debounce = (func,delay) => {
+//     let timer;
+//     return (...params)=>{
+//         if(timer){
+//             clearInterval(timer)
+//         }
+//         timer = setTimeout(()=>{
+//             func(...params)
+//         },delay)
+//     }
+// }
+export const useDebounce = (value,delay)=>{
+    const [debouncedValue,setDebouncedValue] = useState(value)
+    useEffect(()=>{
+        //每次在value变化后进行页面更新，设置一个定时器
+        const timer = setTimeout(()=> setDebouncedValue(value),delay)
+        //在上一个useEffect处理完后在运行，清理定时器
+        return () => clearInterval(timer)
+    },[value,delay])
+    return debouncedValue
 }
