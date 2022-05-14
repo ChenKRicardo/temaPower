1ae51a995e7ad290102ce1fbdb7fc03ee59f1dc9
Ricardo陈 <96281398+CKIVZy@users.noreply.github.com>
2022-05-14 09:36:02 +0800

    chore:安装devtool

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {loadDevTools} from 'jira-dev-tool'
+loadDevTools(()=>{
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>, 
+  document.getElementById('root')
+  );
+})
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
 reportWebVitals();
